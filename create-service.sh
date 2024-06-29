557ba49b299ba7e27ee0f1754ad082bcb2f8ae53
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-29 23:16:13 +0200

    Create create-service.sh

---
+++ create-service.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo tee /etc/systemd/system/0g-storage.service > /dev/null <<EOF
+[Unit]
+Description=0g storage node
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+User=root
+ExecStart=/root/0g-storage-node/target/release/zgs_node --config config.toml
+Restart=always
+RestartSec=30
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable 0g-storage
+
+echo "Service created, start with start-service.sh"
