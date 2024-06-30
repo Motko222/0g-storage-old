eb079aae55712825ef1110e85eca9316f4684b42
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-30 23:11:31 +0200

    Update create-service.sh

--- create-service.sh
+++ create-service.sh
@@ -15,6 +15,21 @@ RestartSec=30
 WantedBy=multi-user.target
 EOF
 
+sudo tee /etc/systemd/system/0g-storage-kv.service > /dev/null <<EOF
+[Unit]
+Description=0g storage kv
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+User=root
+WorkingDirectory=/root/0g-storage-kv/run
+ExecStart=/root/0g-storage-kv/target/release/zgs_kv --config config.toml
+Restart=always
+RestartSec=30
+[Install]
+WantedBy=multi-user.target
+EOF
+
 sudo systemctl daemon-reload
 sudo systemctl enable 0g-storage
 
