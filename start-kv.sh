81f90600ce37984ddb743de60f38aafaaea146fd
Motko <77466238+Motko222@users.noreply.github.com>
2024-06-30 23:51:58 +0200

    Update start-kv.sh

--- start-kv.sh
+++ start-kv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
  
 sudo systemctl restart 0g-storage-kv
-tail -n 100 -f $(find ~/0g-storage-kv/run/log -type f -exec ls -t1 {} + | head -1)
+sudo journalctl -u 0g-storage-kv.service -f --no-hostname -o cat
