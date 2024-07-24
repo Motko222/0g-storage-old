71cd5f70ce4bc48ae65a8cb369326cf1a1db2f21
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-24 16:41:49 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,14 +14,14 @@ sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.0.linux-amd64.ta
 export PATH=$PATH:/usr/local/go/bin
 
 cd ~
-git clone -b v0.3.3 https://github.com/0glabs/0g-storage-node.git
+git clone -b v0.3.4 https://github.com/0glabs/0g-storage-node.git
 cd 0g-storage-node
 git submodule update --init
 cargo build --release
 
 cd ~
 git clone -b v1.1.0-testnet https://github.com/0glabs/0g-storage-kv.git
-cd 0g-storage-node
+cd 0g-storage-kv
 git submodule update --init
 cargo build --release
 
