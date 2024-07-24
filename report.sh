bc4fefbd2133078f0fa6d2ca3b912c6d2b7ce1a3
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-24 18:30:52 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -11,14 +11,17 @@ node_rpc=$(cat ~/0g-storage-node/run/config.toml | grep '^rpc_listen_address =' 
 chain_rpc=$(cat ~/0g-storage-node/run/config.toml | grep '^blockchain_rpc_endpoint =' | tail -1 | awk '{print $3}' | sed 's/"//g')
 kv_rpc=$(cat ~/0g-storage-kv/run/config.toml | grep '^rpc_listen_address =' | tail -1 | awk '{print $3}' | sed 's/"//g')
 
-#get version
+#get storage node info
 cd ~/0g-storage-node/target/release
 node_version=$(./zgs_node --version | awk '{print $2}')
-
-#get storage node info
 json=$(curl -sX POST $node_rpc -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"zgs_getStatus","params":[],"id":1}')
 node_height=$(echo $json | jq -r .result.logSyncHeight)
 peers=$(echo $json | jq -r .result.connectedPeers)
+
+#get kv info
+kv_result=$(curl -sX POST $kv_rpc -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"kv_getStatus","params":[],"id":1}'  | jq .result)
+cd ~/0g-storage-kv/target/release
+kv_version=$(./zgs_kv --version | awk '{print $2}')
 
 #get chain info
 chain_height=$((16#$(curl -s -X POST $chain_rpc  -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq -r  .result | sed 's/0x//g')))
@@ -29,13 +32,15 @@ cat << EOF
   "id":"$ID",
   "machine":"$MACHINE",
   "folder_size":"$folder_size",
-  "node rpc":"$node_rpc",
-  "node version":"$node_version",
-  "node height":"$node_height",
-  "node peers":"$peers",
-  "chain rpc":"$chain_rpc",
-  "chain height":"$chain_height"
-  "kv rpc":"$kv_rpc"
+  "node_rpc":"$node_rpc",
+  "node_version":"$node_version",
+  "node_height":"$node_height",
+  "peers":"$peers",
+  "chain_rpc":"$chain_rpc",
+  "chain_height":"$chain_height"
+  "kv_rpc":"$kv_rpc"
+  "kv_result":"$kv_result"
+  "kv_version":"$kv_version"
 }
 EOF
 
