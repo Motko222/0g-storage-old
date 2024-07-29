9d549dd1d165a9a9dfe0391111ba4efa30928611
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-29 10:08:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,11 @@ sudo apt-get install clang cmake build-essential
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 #install go
+cd ~
 wget https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
+rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
+rm go1.22.5.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 #backup config files
