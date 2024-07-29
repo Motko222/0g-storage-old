1ee6958dd9c42de2750e540ebca732ff523a4368
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-29 10:09:50 +0200

    Update update-node.sh

--- update-node.sh
+++ update-node.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-read -p "tag? (https://github.com/0glabs/0g-storage-node/releases) " tag
-
 cd ~/0g-storage-node
 git stash
-git tag -d $tag
+git tag -d v0.3.4
 git fetch --all --tags
-git checkout 5b6a4c716174b4af1635bfe903cd4f82894e0533
+git checkout 7d73ccd
 git submodule update --init
 cargo build --release
