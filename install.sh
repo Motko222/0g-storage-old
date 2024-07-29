c4d1ec86b24fb7025cf4ebba7740005a33cfffaf
Motko <77466238+Motko222@users.noreply.github.com>
2024-07-29 09:55:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,24 +5,20 @@ case $yn in
  n|N|no|No|NO) exit ;;
 esac
 
+#update
 sudo apt-get update
 sudo apt-get install clang cmake build-essential
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# Download the Go installer
+#install go
 wget https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
-
-# Extract the archive
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
-
-# Add /usr/local/go/bin to the PATH environment variable by adding the following line to your ~/.profile.
 export PATH=$PATH:/usr/local/go/bin
 
+#backup config files
 [ -d ~/backup ] mkdir ~/backup
 [ -d ~/backup/0g-storage-kv ] mkdir ~/backup/0g-storage-kv
 [ -d ~/backup/0g-storage-node ] mkdir ~/backup/0g-storage-node
-
-#backup config files
 cp ~/0g-storage-node/run/config.toml ~/backup/0g-storage-node/config.toml
 cp ~/0g-storage-node/run/log_config ~/backup/0g-storage-node/log_config
 cp ~/0g-storage-kv/run/config.toml ~/backup/0g-storage-kv/config.toml
