19319133859a5875352e378b1ac0a35e0a591d9e
Motko <77466238+Motko222@users.noreply.github.com>
2024-08-23 12:52:27 +0200

    Update install-dependancies.sh

--- install-dependancies.sh
+++ install-dependancies.sh
@@ -6,9 +6,12 @@ sudo apt-get install clang cmake build-essential
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 #install go
-cd ~
-wget https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
-rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
-rm go1.22.5.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
+cd $HOME 
+ver="1.23.0"
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" 
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
