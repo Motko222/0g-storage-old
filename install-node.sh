4cfc9a909ae2ff4ee97c97dc228347a42006b586
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-19 23:17:13 +0200

    Update install-node.sh

--- install-node.sh
+++ install-node.sh
@@ -8,6 +8,9 @@ read -p "Tag ? (https://github.com/0glabs/0g-storage-node/releases) " tag
 cp ~/0g-storage-node/run/config.toml ~/backup/0g-storage-node/config.toml
 cp ~/0g-storage-node/run/log_config ~/backup/0g-storage-node/log_config
 
+#stop
+sudo systemctl stop 0g-storage
+
 #deploy
 cd ~
 rm -r 0g-storage-node
@@ -19,3 +22,7 @@ cargo build --release
 #restore config files
 cp ~/backup/0g-storage-node/config.toml ~/0g-storage-node/run/config.toml
 cp ~/backup/0g-storage-node/log_config ~/0g-storage-node/run/log_config
+
+#start
+sudo systemctl restart 0g-storage
+tail -n 100 -f $(find ~/0g-storage-node/run/log -type f -exec ls -t1 {} + | head -1)
