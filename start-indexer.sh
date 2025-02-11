5ce1e732a1e1dc8f36175cf2b9956e6c66cf9e5a
Motko <77466238+Motko222@users.noreply.github.com>
2025-02-11 22:23:20 +0100

    Update start-indexer.sh

--- start-indexer.sh
+++ start-indexer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $(cd $path | awk -F/ '{print $NF}'))
+
 #read storage servers from indexer setup file
 cd $path
 storage=$(cat indexer)
