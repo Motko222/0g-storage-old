54f92b393b892897fcabbde60fa6bce3f742fffb
Motko <77466238+Motko222@users.noreply.github.com>
2025-02-11 22:17:25 +0100

    Update start-indexer.sh

--- start-indexer.sh
+++ start-indexer.sh
@@ -4,6 +4,9 @@
 cd $path
 storage=$(cat indexer)
 
+#stop
+kill -9 indexer
+
 #start indexer
 cd /root/0g-storage-client
 ./0g-storage-client indexer --endpoint :12345 --trusted $storage &>~/logs/0g-indexer &
