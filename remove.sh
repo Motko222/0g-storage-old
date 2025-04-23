880c207e13eaf3a6c6eb2bdcb0e7f8a1d6f360e1
Motko <77466238+Motko222@users.noreply.github.com>
2025-04-23 09:17:08 +0200

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -6,12 +6,23 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 read -p "Sure? " c
 case $c in y|Y) ;; *) exit ;; esac
 
-systemctl stop $folder.service
-systemctl disable $folder.service
-rm /etc/systemd/system/$folder.service
-rm -r /root/0g-storage-node/run/db
-rm -r /root/0g-storage-node/run/log
-rm -r /root/backup/0g-storage-node 
-mv -f /root/0g-storage-node /root/backup
-rm -r /root/backup/scripts/0g-storage 
+#remove services
+systemctl stop 0g-storage.service
+systemctl disable 0g-storage.service
+rm /etc/systemd/system/0g-storage.service
+systemctl stop 0g-storage-kv.service
+systemctl disable 0g-storage-kv.service
+rm /etc/systemd/system/0g-storage-kv.service
+
+#backup config
+mkdir /root/backup/0g-storage-node
+mv -f /root/0g-storage-node/run/config.toml /root/backup/0g-storage-node
+mv -f /root/0g-storage-node/run/log_config /root/backup/0g-storage-node
+rm -r /root/0g-storage-node
+mkdir /root/backup/0g-storage-kv
+mf -f /root/0g-storage-kv/run/config.toml /root/backup/0g-storage-kv
+rm -r /root/0g-storage-kv
+
+#backup scripts
+rm -r /root/scripts/0g-storage/.git
 mv -f /root/scripts/$folder /root/backup/scripts
